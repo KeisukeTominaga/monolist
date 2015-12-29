534f2c42e812b493228f1ca6b065474815c8dfa1
keisuketominaga <keisuke.tominaga@icloud.com>
2015-12-29 13:21:34 +0000

    12/28

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -12,10 +12,11 @@ class OwnershipsController < ApplicationController
     if @item.new_record?
       begin
         # TODO 商品情報の取得 Amazon::Ecs.item_lookupを用いてください
-        
-        Amazon::Ecs.item_lookup(IdType)
-        
-        response = {}
+
+        response = Amazon::Ecs.item_lookup(params[:asin] , 
+                                    :response_group => 'Medium' , 
+                                    :country => 'jp')        
+
       rescue Amazon::RequestError => e
         return render :js => "alert('#{e.message}')"
       end
