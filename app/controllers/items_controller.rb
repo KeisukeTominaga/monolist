25eca5d7d0b20bc70b9f483399ff0b9c1b71228a
keisuketominaga <keisuke.tominaga@icloud.com>
2016-01-05 08:51:14 +0000

    0104

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,22 +1,25 @@
 class ItemsController < ApplicationController
-  before_action :logged_in_user , except: [:show]
-  before_action :set_item, only: [:show]
+before_action :logged_in_user , except: [:show]
+before_action :set_item, only: [:show]
 
-  def new
-    if params[:q]
-      response = Amazon::Ecs.item_search(params[:q] , 
-                                  :search_index => 'All' , 
-                                  :response_group => 'Medium' , 
-                                  :country => 'jp')
-      @amazon_items = response.items
-    end
-  end
+def new
+if params[:q]
 
-  def show
-  end
+response = Amazon::Ecs.item_search(params[:q] , 
+:search_index => 'All' , 
+:response_group => 'Medium' , 
+:country => 'jp')
 
-  private
-  def set_item
-    @item = Item.find(params[:id])
-  end
+@amazon_items = response.items
+end
+end
+
+def show
+end
+
+private
+def set_item
+@item = Item.find(params[:id])
+end
+
 end
