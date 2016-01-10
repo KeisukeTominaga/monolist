759161f78af1214c91f953f014781500d5b172ef
keisuketominaga <keisuke.tominaga@icloud.com>
2016-01-10 16:55:38 +0000

    0110

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -2,10 +2,12 @@ class RankingController < ApplicationController
   
   def have
     @ranking_haves = Have.group(:item_id).order('count_item_id desc').limit(10).count(:item_id)
+    #@items = Item.find(@ranking_haves).sort_by{|o| @ranking_haves.index(o.id)}
   end
 
   def want
     @ranking_wants = Want.group(:item_id).order('count_item_id desc').limit(10).count(:item_id)
+    
     #@ranking_wants = Want.group(:item_id).limit(10).count(:item_id)
     #@users_id = Want.where(item_id: @ranking_wants[0]).select(users_id)
     
